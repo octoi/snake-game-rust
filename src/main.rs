0536c98f77964dd88060511492fd44cc58ffdc1c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-17 21:49:38 +0530

    imported stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+
 fn main() {
     println!("Hello, world!");
 }
