06a0318d0d8f36a267cb4a7b0414d057d8801538
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-17 22:11:43 +0530

    displayed snake dot

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,18 @@ use piston::window::WindowSettings;
 
 struct Game {
     gl: GlGraphics,
-    // snake: Snake,
+    snake: Snake,
 }
 
 impl Game {
-    fn render(&mut self, arg: &RenderArgs) {
-        // use graphics;
+    fn render(&mut self, args: &RenderArgs) {
+        let green_color: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
 
-        let GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-
-        self.gl.draw(arg.viewport(), |_c, gl| {
-            graphics::clear(GREEN, gl);
+        self.gl.draw(args.viewport(), |_c, gl| {
+            graphics::clear(green_color, gl);
         });
+
+        self.snake.render(&mut self.gl, args);
     }
 }
 
@@ -34,7 +34,15 @@ struct Snake {
 
 impl Snake {
     fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
-        // use graphics;
+        let red_color: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 20_f64);
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            graphics::rectangle(red_color, square, transform, gl);
+        });
     }
 }
 
@@ -49,6 +57,10 @@ fn main() {
 
     let mut game = Game {
         gl: GlGraphics::new(opengl),
+        snake: Snake {
+            pos_x: 50,
+            pos_y: 50,
+        },
     };
 
     let mut events = Events::new(EventSettings::new()).ups(10);
