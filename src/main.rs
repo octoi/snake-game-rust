37d1f94cc04d62b7b851bbcfb02123b3a86cac70
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-17 22:14:48 +0530

    snake direction

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,13 @@ use piston::event_loop::*;
 use piston::input::*;
 use piston::window::WindowSettings;
 
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
 struct Game {
     gl: GlGraphics,
     snake: Snake,
@@ -30,13 +37,15 @@ impl Game {
 struct Snake {
     pos_x: i32,
     pos_y: i32,
+    dir: Direction,
 }
 
 impl Snake {
     fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
         let red_color: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 20_f64);
+        let square =
+            graphics::rectangle::square((self.pos_x * 20) as f64, (self.pos_y * 20) as f64, 20_f64);
 
         gl.draw(args.viewport(), |c, gl| {
             let transform = c.transform;
@@ -58,8 +67,9 @@ fn main() {
     let mut game = Game {
         gl: GlGraphics::new(opengl),
         snake: Snake {
-            pos_x: 50,
-            pos_y: 50,
+            pos_x: 0,
+            pos_y: 0,
+            dir: Direction::Right,
         },
     };
 
